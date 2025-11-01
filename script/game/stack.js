01928254d7f21729785ac055b8d1bc90ada01a28
Sub2Ninah <70987554+krsofficial@users.noreply.github.com>
2025-11-02 02:48:05 +0800

    hotfix

--- script/game/stack.js
+++ script/game/stack.js
@@ -120,6 +120,10 @@ export default class Stack extends GameModule {
 				this.grid[x][y] = "goldgem"
 			} else if (this.grid[x][y] === "frozen") {
 				this.grid[x][y] = "icegem"
+			} else if (this.grid[x][y] === "black") {
+				this.grid[x][y] = "blackgem"
+			} else if (this.grid[x][y] === "white") {
+				this.grid[x][y] = "whitegem"
 			} else if (this.grid[x][y] === color) {
 				this.grid[x][y] = `${color}gem`
 			}
