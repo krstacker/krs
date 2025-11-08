a09282682209141bf7acd270bde432fe1d441287
Sub2Ninah <70987554+krstacker@users.noreply.github.com>
2025-11-09 00:54:16 +0800

    Update stack.js

--- script/game/stack.js
+++ script/game/stack.js
@@ -1678,6 +1678,7 @@ export default class Stack extends GameModule {
         for (let shiftY = y; shiftY >= 0; shiftY--) { 
 		  if (this.noFrozenMinos() === true) {
 			this.grid[x][shiftY] = this.grid[x][shiftY - 1]
+			this.lastPlacedBlocks[x][shiftY] = this.lastPlacedBlocks[x][shiftY - 1]
 			if (
 				this.grid[x][shiftY] != null &&
 				this.grid[x][shiftY - 1] != null
@@ -1687,6 +1688,7 @@ export default class Stack extends GameModule {
 			this.dirtyCells.push([x, shiftY + 1])
 		  } else if (y === bottomLine && this.lineClear >= 4) {
 			this.grid[x][shiftY] = this.grid[x][shiftY - 1]
+			this.lastPlacedBlocks[x][shiftY] = this.lastPlacedBlocks[x][shiftY - 1]
 			if (
 				this.grid[x][shiftY] != null &&
 				this.grid[x][shiftY - 1] != null
@@ -1709,6 +1711,7 @@ export default class Stack extends GameModule {
       for (let x = 0; x < this.grid.length; x++) {
         for (let shiftY = y; shiftY >= 0; shiftY--) {
           this.grid[x][shiftY] = this.grid[x][shiftY - 1]
+		  this.lastPlacedBlocks[x][shiftY] = this.lastPlacedBlocks[x][shiftY - 1]
           if (
             this.grid[x][shiftY] != null &&
             this.grid[x][shiftY - 1] != null
